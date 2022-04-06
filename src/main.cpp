257964d4d7567dd52eb0e90ee0c3f5f27f618473
Moritz Moxter <moritz@clockworks.io>
2022-04-06 19:09:47 +0200

    feat: initial

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ *
+ *  File:           main.cpp
+ *
+ *  Function:       t.b.d.
+ *
+ *  Copyright:      Copyright (c) 2022 Clockworks UG c/o Moritz Moxter <moritz@clockworks.io
+ *
+ *  License:        Proprietary Software.
+ *
+ *  Author:         Moritz Moxter
+ *
+ *  Description:    t.b.d.
+ *
+ ******************************************************************************/
+
+#include "main.h"
+
+
+/**
+ */
+void setup() {
+
+}
+
+/**
+ */
+void loop() {
+
+}
---
+++ src/main.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ *
+ *  File:           main.h
+ *
+ *  Function:       t.b.d.
+ *
+ *  Copyright:      Copyright (c) 2022 Clockworks UG c/o Moritz Moxter <moritz@clockworks.io
+ *
+ *  License:        Proprietary Software.
+ *
+ *  Author:         Moritz Moxter
+ *
+ *  Description:    t.b.d.
+ *
+ ******************************************************************************/
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 0
+#define NUM_LED 97
+#define DELAYVAL 200
