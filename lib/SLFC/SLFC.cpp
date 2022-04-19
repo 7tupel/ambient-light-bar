a81c351e3a5a27b95d108a5c4f4101762fc44c7c
Moritz Moxter <moritz@clockworks.io>
2022-04-19 20:06:57 +0200

    chore: intermediate

---
+++ lib/SLFC/SLFC.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ *
+ *  File:           SLFC.cpp
+ *
+ *  Function:       t.b.d.
+ *
+ *  Copyright:      Copyright (c) 2022 Clockworks UG c/o Moritz Moxter <moritz@clockworks.io
+ *
+ *  License:        Proprietary Software.
+ *
+ *  Author:         Moritz Moxter
+ *
+ *  Description:    t.b.d.
+ *
+ ******************************************************************************/
+#include "SLFC.h"
---
+++ lib/SLFC/SLFC.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *
+ *  File:           SLFC.h
+ *
+ *  Function:       t.b.d.
+ *
+ *  Copyright:      Copyright (c) 2022 Clockworks UG c/o Moritz Moxter <moritz@clockworks.io
+ *
+ *  License:        Proprietary Software.
+ *
+ *  Author:         Moritz Moxter
+ *
+ *  Description:    t.b.d.
+ *
+ ******************************************************************************/
+
+ #ifndef SLFC_h
+ #define SLFC_h
+
+/**
+ *
+ */
+class SLFC {
+  private:
+  public:
+    SLFC();
+}
+
+#endif
